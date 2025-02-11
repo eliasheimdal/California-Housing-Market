cacfb8e99ae0d34c6fd7751d2fccecef96032c7d
Elias <eliaswhe@stud.ntnu.no>
2025-02-11 20:20:06 +0100

    models saved

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main.py
 from src.data_preparation import load_data, preprocess_data
 from src.model import train_model, evaluate_model, tune_model
+import pickle
 
 def main():
     # Load and preprocess data
@@ -17,6 +18,11 @@ def main():
     tuned_model = tune_model(X_train, y_train)
     print("\nEvaluating tuned model...")
     evaluate_model(tuned_model, X_test, y_test)
+
+    with open('models/scaler.pkl', 'wb') as f:
+        pickle.dump(scaler, f)
+    with open('models/tuned_model.pkl', 'wb') as f:
+        pickle.dump(tuned_model, f)
 
 if __name__ == "__main__":
     main()
